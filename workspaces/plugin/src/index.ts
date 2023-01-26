767c0a446fc3785a124dd033580993aec7e40baf
jiadesen <jiadesen1993@163.com>
2023-01-26 19:39:33 +0800

    feat(plugin): integrate `html-minifier-terser`

--- workspaces/plugin/src/index.ts
+++ workspaces/plugin/src/index.ts
@@ -1,3 +1,23 @@
+import type { AllowedEvent, MpaOptions } from './api-types';
+import type { Plugin } from 'vite';
+import { createMpaPlugin as mpaPlugin } from './plugin';
+import { htmlMinifyPlugin } from './html-minify';
+
 export * from './api-types';
-export * from './plugin';
 export * from './utils';
+
+export function createMpaPlugin<
+  PN extends string,
+  PFN extends string,
+  PT extends string,
+  Event extends AllowedEvent,
+  TPL extends string,
+>(
+  config: MpaOptions<PN, PFN, PT, Event, TPL>,
+): Plugin[] {
+  const { htmlMinify } = config;
+  if (!htmlMinify) {
+    return [mpaPlugin(config)];
+  }
+  return [mpaPlugin(config), htmlMinifyPlugin(htmlMinify === true ? {} : htmlMinify)];
+}
