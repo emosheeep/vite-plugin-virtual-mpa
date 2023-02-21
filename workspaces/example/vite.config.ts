9cf60b1b513738ea864b9efe08609671502f3031
秦旭洋 <qinxuyang@bytedance.com>
2023-02-21 14:13:26 +0800

    feat: Add preview server's history fallback support.

--- workspaces/example/vite.config.ts
+++ workspaces/example/vite.config.ts
@@ -48,6 +48,8 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
+  server: { port: 5173 },
+  preview: { port: 5173 },
   plugins: [
     vue(),
     vueJsx(),
@@ -63,7 +65,7 @@ export default defineConfig({
        */
       htmlMinify: true,
       /**
-       * Customize the history fallback rewrite rules.
+       * Customize the history fallback rewrite rules for `dev server`.
        * If you config your pages as above, this rewrite rules will be automatically generated.
        * Otherwise you should manually write it, which will overwrite the default.
        */
@@ -74,6 +76,14 @@ export default defineConfig({
           ),
           to: (ctx) => normalizePath(`/${base}/fruits/${ctx.match[1]}.html`),
         },
+      ],
+      /**
+       * Customize the history fallback rewrite rules for `preview server`.
+       * This option is almost the same with `rewrites`.
+       */
+      previewRewrites: [
+        // If there's no index.html, you need to manually set rules for history fallback like:
+        { from: /.*/, to: '/home.html' },
       ],
       /**
        * Sometimes you might want to reload `pages` config or restart ViteDevServer when
