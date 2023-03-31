e90cd5dbb16f271e70e38eb54d8c5f13aeef9858
jiadesen <jiadesen1993@163.com>
2023-04-01 01:04:50 +0800

    feat(plugin): reserve user's right to set `clearScreen` to `true`

--- workspaces/plugin/src/plugin.ts
+++ workspaces/plugin/src/plugin.ts
@@ -143,12 +143,12 @@ export function createMpaPlugin<
 
   return {
     name: pluginName,
-    config() {
+    config({ clearScreen }) {
       configInit(pages); // Init
 
       return {
         appType: 'mpa',
-        clearScreen: false,
+        clearScreen: clearScreen ?? false,
         optimizeDeps: {
           entries: pages
             .map(v => v.entry)
