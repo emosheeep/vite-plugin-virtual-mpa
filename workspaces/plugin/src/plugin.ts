da0c4b7ba0e029ef3feaf84bfcd2458981409b24
jiadesen <jiadesen1993@163.com>
2023-04-02 17:15:42 +0800

    Revert "chore(plugin): remove log"
    This reverts commit 4ddb12cca6af7a1b37fd1371bf3598c5d70fd905.

--- workspaces/plugin/src/plugin.ts
+++ workspaces/plugin/src/plugin.ts
@@ -103,6 +103,8 @@ export function createMpaPlugin<
   function getTemplateContent(filename: string) {
     const page = virtualPageMap[filename];
 
+    console.log(page);
+
     try {
       return fs.readFileSync(page.template || template, 'utf-8');
     } catch (e) {
