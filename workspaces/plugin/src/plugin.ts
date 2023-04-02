2e2880d75929c14707725897b15670801c8dafa4
jiadesen <jiadesen1993@163.com>
2023-04-02 16:28:16 +0800

    refactor(plugin): remove the `load` hook, use the 'getTemplateContent' method instead

--- workspaces/plugin/src/plugin.ts
+++ workspaces/plugin/src/plugin.ts
@@ -98,6 +98,25 @@ export function createMpaPlugin<
   }
 
   /**
+   * Get html according to page configurations.
+   */
+  function getTemplateContent(filename: string) {
+    const page = virtualPageMap[filename];
+
+    console.log(page);
+
+    try {
+      return fs.readFileSync(page.template || template, 'utf-8');
+    } catch (e) {
+      if (verbose) {
+        console.log(
+          `[${pluginName}]: ${color.red(`Failed to load the template of the page '${page.name}'\n${e}`)}`,
+        );
+      }
+    }
+  }
+
+  /**
    * Template file transform.
    */
   function ejsRender(fileContent: string, id?: string) {
@@ -151,14 +170,6 @@ export function createMpaPlugin<
         console.log(`[${pluginName}]: Generated virtual files: \n${inputFiles.join('\n')}`);
       }
     },
-    /**
-     * Get html according to page configurations.
-     */
-    load(id) {
-      const page = virtualPageMap[id];
-      if (!page) return null;
-      return fs.readFileSync(page.template || template, 'utf-8');
-    },
     transformIndexHtml: {
       enforce: 'pre',
       transform(html) {
@@ -170,7 +181,6 @@ export function createMpaPlugin<
         config,
         watcher,
         middlewares,
-        pluginContainer,
         transformIndexHtml,
       } = server;
 
@@ -239,13 +249,11 @@ export function createMpaPlugin<
         }
 
         // load file
-        let loadResult = await pluginContainer.load(fileName);
+        const loadResult = getTemplateContent(fileName);
         if (!loadResult) {
-          throw new Error(`Failed to load url ${fileName}`);
+          // Hand it over to vite.
+          return next();
         }
-        loadResult = typeof loadResult === 'string'
-          ? loadResult
-          : loadResult.code;
 
         /**
          * The following 2 lines fixed #12.
