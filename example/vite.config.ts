4bea1b02c688d8ad38a2439a7fb177202c29d32b
James Camilleri <james@james.mt>
2025-10-20 18:17:37 +0100

    Simplify plugin config

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -36,18 +36,18 @@ const pages = createPages([
   },
   {
     name: 'home',
-    filename: 'fruits/home.html',
+    filename: 'index.html',
     entry: '/src/pages/home/index.js',
     data: {
       title: 'This is Home page',
     },
   },
   {
-    name: 'home2',
-    filename: 'fruits/home2.html',
-    entry: '/src/pages/home2/index.js',
+    name: 'some-test-url',
+    filename: 'index.html',
+    entry: '/src/pages/home/index.js',
     data: {
-      title: 'This is Home2 page',
+      title: 'This is Home page',
     },
   },
 ]);
@@ -60,61 +60,7 @@ export default defineConfig({
     vueJsx(),
     createMpaPlugin({
       template: 'src/template.html',
-      /**
-       * You can write directly or use `createPages` function independently outside and then pass it to this field.
-       * Both of the above can enjoy type hints.
-       */
       pages,
-      /**
-       * The following `scanOptions` configs can replace the `pages` above, but except data injection.
-       */
-      scanOptions: {
-        scanDirs: 'src/pages',
-        entryFile: 'index.js',
-        filename: (name) => `fruits/${name}.html`,
-        template: '../../template.html',
-      },
-      /**
-       * Use html minimization feature at build time.
-       */
-      htmlMinify: true,
-      /**
-       * Customize the history fallback rewrite rules for `dev server`.
-       * If you config your pages as above, this rewrite rules will be automatically generated.
-       * Otherwise you should manually write it, which will overwrite the default.
-       */
-      // rewrites: [
-      //   {
-      //     from: new RegExp(
-      //       normalizePath(`/${base}/(apple|banana|strawberries|home2|home)`),
-      //     ),
-      //     to: (ctx) => normalizePath(`/${base}/fruits/${ctx.match[1]}.html`),
-      //   },
-      // ],
-      /**
-       * Customize the history fallback rewrite rules for `preview server`.
-       * This option is almost the same with `rewrites`.
-       */
-      previewRewrites: [
-        // If there's no index.html, you need to manually set rules for history fallback like:
-        { from: /.*/, to: '/home.html' },
-      ],
-      /**
-       * Sometimes you might want to reload `pages` config or restart ViteDevServer when
-       * there are some files added, removed, changed and so on. You can set `watchOptions` to
-       * customize your own logic.
-       *
-       * The `include` and `exclude` based on `Rollup.createFilter`
-       * @see https://vitejs.dev/guide/api-plugin.html#filtering-include-exclude-pattern
-       */
-      watchOptions: {
-        events: ['add', 'unlink', 'change'],
-        include: ['**/pages/**', '**/infos/**'],
-        handler: (ctx) => {
-          console.log(ctx.type, ctx.file);
-          // ctx.reloadPages();
-        },
-      },
       transformHtml(html, ctx) {
         return {
           html,
